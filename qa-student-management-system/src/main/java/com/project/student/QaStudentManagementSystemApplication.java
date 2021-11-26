d318406c4f3dc778ee921422e6a531252f78867a
Donielll <91885788+Donielll@users.noreply.github.com>
2021-11-26 08:59:56 +0000

    Add files via upload
    Adding my spring project file. THERE HAS TO BE A BETTER WAY TO DO THIS

---
+++ qa-student-management-system/src/main/java/com/project/student/QaStudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaStudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaStudentManagementSystemApplication.class, args);
+	}
+
+}
---
+++ qa-student-management-system/src/main/java/com/project/student/entity/Student.java
@@ -0,0 +1,19 @@
+package com.project.student.entity;
+
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	public Student() {
+
+}
